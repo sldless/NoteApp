5d250faa135b70a5c1f1464a6eecd63105dfc884
sldless <67599596+sldless@users.noreply.github.com>
2022-03-15 13:17:48 -0400

    Removed it because it was uselsss

--- test.js
+++
@@ -1,4 +0,0 @@
-const { Config } = require('node-json-db/dist/lib/JsonDBConfig')
-const { JsonDB } = require('node-json-db');
-var db = new JsonDB(new Config("myDataBase", true, false, '/'));
-db.push('/e', {'a': 'e'}, false)
